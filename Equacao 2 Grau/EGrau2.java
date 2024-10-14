b9c9877411ba1dcb2ea908fce1b96dcf04ebc773
Victor Hugo Silva <90207063+VictorHugoSDev@users.noreply.github.com>
2024-10-14 17:29:53 -0300

    Create EGrau2.java

---
+++ Equacao 2 Grau/EGrau2.java
@@ -0,0 +1,21 @@
+public class EGrau2 {
+    public double a, b, c;
+
+    public EGrau2(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double delta(){
+        return (b*b) - (4*a*c);
+    }
+
+    public double calculaX1(double delta){
+        return (-b + Math.sqrt(delta))/(2*a);
+    }
+
+    public double calculaX2(double delta){
+        return (-b - Math.sqrt(delta))/(2*a);
+    }
+}
